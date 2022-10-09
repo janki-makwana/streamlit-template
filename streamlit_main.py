a8ed36937171d1377a4047427f88ac078dcb6efb
Janki Makwana <jankimakwana123@outlook.com>
2022-10-09 01:24:03 +0100

    final

--- streamlit_main.py
+++ streamlit_main.py
@@ -3,6 +3,7 @@
 Here's our first attempt at using data to create a table:
 """
 
+from ssl import PROTOCOL_TLSv1_2
 import streamlit as st
 import pandas as pd
 import altair as alt
@@ -19,6 +20,9 @@ import seaborn as sns
 import numpy as np
 from nltk.util import ngrams
 from collections import Counter
+from statistics import mean
+from PIL import Image
+
 
 
 def clean_me(tweet):
@@ -65,6 +69,53 @@ dates = data_sentiment['date']
 data['cleaned'] = data['content'].apply(clean_me)
 
 
+def average_me(column):
+    pos_likes = []
+    neg_likes = []
+    neu_likes = []
+
+    for x in range(len(data['sentiment_label'])): 
+        if data['sentiment_label'][x] =='Positive': 
+            pos_likes.append(data[column][x])
+        elif data['sentiment_label'][x] =='Negative': 
+            neg_likes.append(data[column][x])
+        else: 
+            neu_likes.append(data[column][x])
+
+    pos_average = mean(pos_likes)
+    neg_average = mean(neg_likes)
+    neu_average = mean(neu_likes)
+    return pos_average,neg_average,neu_average
+
+
+
+def styling(column_name, styled_column, col):
+    pos_like_ave, neg_like_ave, neu_like_ave = average_me(column_name)
+
+    if (pos_like_ave > neg_like_ave) and (pos_like_ave > neu_like_ave):
+        col.header(styled_column)
+        styled_column = styled_column.lower()
+        col.write(f':sparkles: Average {styled_column} for a positive tweet: {pos_like_ave} :sparkles:')
+        col.write(f'Average {styled_column} for a negative tweet: {neg_like_ave}')
+        col.write(f'Average {styled_column} for a neutral tweet: {neu_like_ave}')
+    elif (neg_like_ave > neu_like_ave) and (neg_like_ave > pos_like_ave):
+        col.header(styled_column)
+        styled_column = styled_column.lower()
+        col.write(f'Average {styled_column} for a positive tweet: {pos_like_ave} ')
+        col.write(f':sparkles: Average {styled_column} for a negative tweet: {neg_like_ave} :sparkles:')
+        col.write(f'Average {styled_column} for a neutral tweet: {neu_like_ave}')
+    else: 
+        col.header(styled_column)
+        styled_column = styled_column.lower()
+        col.write(f'Average {styled_column} for a positive tweet: {pos_like_ave} ')
+        col.write(f'Average {styled_column} for a negative tweet: {neg_like_ave} ')
+        col.write(f':sparkles: Average {styled_column} for a neutral tweet: {neu_like_ave} :sparkles:')
+        
+
+
+
+st.set_page_config(layout="wide")
+
 st.title('Twitter Analysis')
 def basic_skeleton() -> tuple:
     """Prepare the basic UI for the app"""
@@ -82,15 +133,31 @@ def main():
     # add high level site inputs
     text_input = basic_skeleton()
     if text_input:
+
+        # my_bar_graph_one = st.sidebar.progress(0)
+
+        # for percent_complete in range(100):
+        #     time.sleep(0.1)
+        #     my_bar_graph_one.progress(percent_complete + 1)
+    
+        
         st.write('The twitter username you have chosen is: ',text_input)
+        st.write('The total number of tweets', len(data))
         # st.line_chart(data_sentiment.rename(columns={'date':'index'}).set_index('index'))
+
+        col1,col15,col155, col2,col25,col255 = st.columns((0.5,2,0.5,0.5,2,0.5))
+        styling('likeCount', 'Likes',col15)
+        styling('retweetCount', 'Retweets',col25)
+
+        col3, col4 = st.columns((2,2))
+        col15.header('Sentiment over time')
 
         c = alt.Chart(data_sentiment).mark_circle().encode(
         x=alt.X('date', axis=alt.Axis( title='Date')),
         y=alt.Y('sentiment_number', axis=alt.Axis(title='Sentiment')),color='sentiment_label')
 
         # x='date', y='sentiment_number')
-        st.altair_chart(c, use_container_width=True)
+        col3.altair_chart(c, use_container_width=True)
 
                 # Pie chart, where the slices will be ordered and plotted counter-clockwise:
         pos = 0
@@ -106,8 +173,6 @@ def main():
     
         labels = ['Positive', 'Negative', 'Neutral']
         sizes =[pos,neg,neu]
-        explode = (0, 0.1, 0)  # only "explode" the 2nd slice (i.e. 'Hogs')
-
         # fig1, ax1 = plt.subplots()
         # ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
         #         shadow=False, startangle=90)
@@ -125,15 +190,18 @@ def main():
 
         
 
-        st.header("Pie chart")
-        st.plotly_chart(fig)
+        col25.header("Pie chart")
+        col4.plotly_chart(fig)
         # st.write(list_of_things)
         # st.write(list_of_things[0][0])
         # # unique_string=(" ").join(list_of_sep)
+        st.header('WordCloud')
+        col5, col6,col7 = st.columns((1,2,1))
 
         unigrams=documentNgrams(data['cleaned'],1)
         bigrams=documentNgrams(data['cleaned'],2)
         trigrams=documentNgrams(data['cleaned'],3)
+        
 
         wordcloud = WordCloud(mode = "RGBA", background_color=None,width = 1000, height = 500).generate(str(unigrams['words']))
         st.set_option('deprecation.showPyplotGlobalUse', False)
@@ -142,11 +210,11 @@ def main():
         plt.imshow(wordcloud, interpolation='bilinear')
         plt.axis("off")
         plt.show()
-        st.pyplot()
+        col6.pyplot()
 
 
 
-        st.header(f'Whats @{text_input} been talking about')
+        st.header(f'What has @{text_input} been talking about')
         figsns = plt.figure(figsize=(15,5))
         plt.subplots_adjust(wspace=1)
         ax = figsns.add_subplot(131)
@@ -168,6 +236,10 @@ def main():
         plt.xlabel('Count')
 
         st.pyplot(figsns)
+
+
+
+
 
 
 
